3240e4d2efea0b7630e88e70a15f4eab50a132b2
Kira Toal <kiratoal0@gmail.com>
2020-05-28 13:12:01 -0700

    Adds dropdown

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,7 +12,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-let root = document.documentElement;
+let eerieModeOn = false; 
+
+/**
+ * Shows and hides "Eerie Mode" elements
+ */
+const extraContent = document.querySelector("#extra-content");
+function extrasAreVisible(visible) {
+    if (visible) {
+        extraContent.style.display = "block"; 
+        extraContent.style.opacity = "1";  
+        //TODO: fix below line, which doesn't transition the opacity
+        extraContent.classList.add("change-extra-content-opacity"); 
+
+    } else {
+        extraContent.style.display = "none"; 
+    }
+}
+
+const root = document.documentElement;
 
 const secretText = document.querySelector("#secret-text"); 
 const mePic = document.querySelector("#me-pic"); 
@@ -23,6 +41,7 @@ const mainHeading = document.querySelector("#main-heading");
 const mainText = document.querySelector(".main-text");
 window.addEventListener('load', e => {
     mainText.classList.add("fade-text-in"); 
+    extrasAreVisible(false); 
 })
 
 /**
@@ -41,25 +60,24 @@ let messageIndex = 0;
 /**
  * Animates the mouse as it interacts with section headers. 
  */
-
 const sections = document.querySelectorAll(".sections li");
 sections.forEach(section => {
     section.addEventListener("mouseover", e => {
-        circleAroundMouse.classList.add("grow-cursor"); 
-        section.classList.add("color-text"); 
+        circleAroundMouse.classList.add("grow-cursor");
+        if (eerieModeOn) { section.classList.remove("eerie-text"); } 
+        section.classList.add("color-text");  
     }); 
     section.addEventListener("mouseleave", e => {
         circleAroundMouse.classList.remove("grow-cursor");
+        if (eerieModeOn) { section.classList.add("eerie-text"); } 
         section.classList.remove("color-text"); 
     }); 
     section.addEventListener("click", e => {
-        console.log(messageIndex); 
         if (section.id === "secret-text") {
             mainText.textContent = secretMessages[messageIndex]; 
             if (messageIndex < secretMessages.length) {
                 messageIndex += 1; 
             } else {
-                // profilePicture.classList.add("fade-image-out"); 
                 activateEerieMode(); 
             }
         } else {
@@ -70,7 +88,6 @@ sections.forEach(section => {
 
 document.addEventListener('keydown', function(event) {
     if(event.keyCode == 39) {
-        console.log("right key pressed");
         activateEerieMode(); 
     }
 });
@@ -79,17 +96,22 @@ document.addEventListener('keydown', function(event) {
  * Changes the theme of the page 
  */
 function activateEerieMode() {
-    console.log("eerie mode activated"); 
+    eerieModeOn = true; 
     mePic.classList.add("fade-image-out"); 
     document.body.style.backgroundColor = "black"; 
     root.style.setProperty('--target-color', "red");
+    root.style.setProperty('--circle-cursor-color', "red"); 
+    root.style.setProperty('--highlighted-text-color', "white"); 
     mainHeading.classList.add("eerie-text"); 
     sections.forEach(section => {
         section.classList.add("eerie-text"); 
     });
     mainHeading.textContent = "Something Fun"; 
+    mainText.textContent = mainText.dataset.reversed; 
     mainText.classList.add("eerie-text"); 
+    extrasAreVisible(true); 
 }
+
 
 /**
  * Recommends a movie based on dropdown input 
@@ -100,11 +122,12 @@ let userSelectedValue;
 spnMovies = ["Hereditary", "It Follows", "The Exorcist", "Ju-On: The Grudge", "Ringu"]
 nonSpnMovies = ["Midsommar", "The Shining", "Hush", "The Strangers", "Orphan"]
 
+const result = document.querySelector("#result");
 function recommendMovie() {
     userSelectedValue = dropdown.options[dropdown.selectedIndex].value; 
     if (userSelectedValue === "spn") {
-        console.log(spnMovies[Math.floor(Math.random() * spnMovies.length)]); 
-    } else {
-        console.log(nonSpnMovies[Math.floor(Math.random() * nonSpnMovies.length)]); 
+        result.textContent = spnMovies[Math.floor(Math.random() * spnMovies.length)]; 
+    } else { 
+        result.textContent = nonSpnMovies[Math.floor(Math.random() * nonSpnMovies.length)]; 
     }
 }
