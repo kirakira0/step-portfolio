f856ec90b66945eb2bc208800e9c044f09e8010a
Kira Toal <kiratoal0@gmail.com>
2020-06-02 20:58:18 +0000

    Fixes comment indentation

--- portfolio/src/main/webapp/greeting.js
+++ portfolio/src/main/webapp/greeting.js
@@ -2,11 +2,10 @@
  * Uses arrow functions to fetch and display greeting from the data servlet.
  */
 function getFirstGreeting() {
-
-	fetch('/data').then(response => response.json()).then((greetings) => {
+  fetch('/data').then(response => response.json()).then((greetings) => {
     // greetings is an object, not a string, so we have to reference its fields to create HTML content
-		const greetingsListElement = document.getElementById('greeting');
-		greetingsListElement.innerHTML = greetings[0];
-	}); 
+    const greetingsListElement = document.getElementById('greeting');
+    greetingsListElement.innerHTML = greetings[0];
+  }); 
 
 }
