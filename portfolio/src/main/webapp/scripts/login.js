1ceba394a3cab40c703b4d5ba47d02129e7475a4
Kira Toal <kiratoal0@gmail.com>
2020-06-09 22:51:28 +0000

    Adds error handling for async functions

--- portfolio/src/main/webapp/scripts/login.js
+++ portfolio/src/main/webapp/scripts/login.js
@@ -2,26 +2,38 @@
  * Shows feedback on login status
  */
 async function showCommentFeedback() {
-  const response = await fetch('/create-new-comment');
-  const loginStatus = await response.text();
-  document.getElementById('login-feedback').innerText = loginStatus;
+  try {
+    const response = await fetch('/create-new-comment');
+    const loginStatus = await response.text();
+    document.getElementById('login-feedback').innerText = loginStatus;
+  } catch {
+    alert('Failed to fetch login status.'); 
+  }
+
 }
 
 /**
  * Changes button text to reflect whether user should log in or out
  */
 async function updateLoginStatusOnButton() {
-  const response = await fetch('/check-login-status');
-  const loginStatus = await response.text();
-  document.getElementById('login-button').innerText = loginStatus;
+  try {
+    const response = await fetch('/check-login-status');
+    const loginStatus = await response.text();
+    document.getElementById('login-button').innerText = loginStatus;
+  } catch {
+    document.getElementById('login-button').innerText = "Log in/out"; 
+  }
 }
 
 /**
  * Redirect the user to the login page 
  */
 async function redirectToLoginPage() {
-  const response = await fetch('/login');
-  const loginStatus = await response.text();
-  window.location.replace("/login");
+  try {
+    const response = await fetch('/login');
+    const loginStatus = await response.text();
+    window.location.replace("/login");
+  } catch {
+    alert('Failed to load login page.'); 
+  }
 }
-
