7b4785a31494a583ae077525f9c79f6a50a5f116
Kira Toal <kiratoal0@gmail.com>
2020-06-10 23:06:42 +0000

    Alphabetizes imports

--- portfolio/src/main/java/com/google/sps/servlets/CreateNewComment.java
+++ portfolio/src/main/java/com/google/sps/servlets/CreateNewComment.java
@@ -17,14 +17,14 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.gson.Gson;
+import com.google.sps.servlets.models.Comment;
 import java.io.IOException;
+import java.util.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
-import com.google.gson.Gson;
-import com.google.sps.servlets.models.Comment;
 
 /*
  * Servlet responsible for creating new comments 
@@ -43,5 +43,5 @@ public class CreateNewComment extends HttpServlet {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService(); // create instance of DatastoreService class
     datastore.put(commentEntity);
   }
-  
+
 }
