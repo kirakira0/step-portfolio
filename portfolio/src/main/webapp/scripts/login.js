a5aaffe6b7359f05a6b6af764e534385d9cd8d5c
Kira Toal <kiratoal0@gmail.com>
2020-06-18 05:37:23 +0000

    Combined redundant methods

--- portfolio/src/main/webapp/scripts/login.js
+++ portfolio/src/main/webapp/scripts/login.js
@@ -1,42 +1,29 @@
 /**
  * Shows feedback on login status
  */
-async function showCommentFeedback() {
+async function greetAccordingToLoginStatus() {
   try {
     const response = await fetch('/check-login-status');
     const user = await response.json();
+
+    const welcomeText = document.getElementById('welcome-text'); 
+    const loginButton = document.getElementById('login-button'); 
+
     if (user.map.loggedIn) {
       const emailAddress = user.map.email; 
       const name = emailAddress.substring(0, emailAddress.indexOf('@')); 
       document.getElementById('welcome-text').innerText = `Hello ${name}! ` + 
-        "Now that you're logged in,feel free to leave a comment."; 
+        "Now that you're logged in, feel free to leave a comment."; 
+      loginButton.innerText = "Log out"; 
     } else {
       document.getElementById('welcome-text').innerText = "Welcome! Please log " +
         "in to leave a comment."; 
+      loginButton.innerText = "Log in"; 
     }
-    updateLoginStatusOnButton(); 
   } catch {
     alert('Failed to fetch login status.'); 
   }
 
-}
-
-/**
- * Changes button text to reflect whether user should log in or out
- */
-async function updateLoginStatusOnButton() {
-  const loginButton = document.getElementById('login-button'); 
-  try {
-    const response = await fetch('/check-login-status');
-    const user = await response.json(); 
-    if (user.map.loggedIn) {
-      loginButton.innerText = "Log out";
-    } else {
-      loginButton.innerText = "Log in";
-    }
-  } catch {
-    loginButton.innerText = "Log in"; 
-  }
 }
 
 /**
